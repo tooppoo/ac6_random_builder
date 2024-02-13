e1b1f05917e787771dbb928d49fb80fa24b57c7c
philomagi <tooppoo56@gmail.com>
2024-02-14 03:01:45 +0900

    aliasが正しく読み込まれないケースがあるので削除

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
+import {join} from "path";
 import {defineConfig} from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
+  resolve: {
+    alias: {
+      '~/': join(__dirname, '/'),
+    }
+  }
 })

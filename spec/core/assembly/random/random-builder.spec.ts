2932a6338e350129498232e2772faf56faeb7385
philomagi <tooppoo56@gmail.com>
2024-02-16 01:37:32 +0900

    ランダム生成実装

---
+++ spec/core/assembly/random/random-builder.spec.ts
@@ -0,0 +1,32 @@
+import {booster, boosterNotEquipped} from "data/types/base/classification";
+import fc from 'fast-check'
+import {candidates} from "~core/assembly/candidates";
+import {randomBuild} from "~core/assembly/random/random-builder";
+import {describe, expect, it} from 'vitest'
+
+describe(randomBuild.name, () => {
+  fc.assert(
+    fc.property(fc.float({ min: 0, max: 1 }), (i) => {
+      const actual = randomBuild(candidates, () => i)
+
+      if (actual.legs.category === 'tank') {
+        describe('with tank', () => {
+          describe(actual.legs.name, () => {
+            it('should not contain booster', () => {
+              expect(actual.booster.classification).toStrictEqual(boosterNotEquipped)
+            })
+          })
+        })
+      } else {
+        describe('without tank', () => {
+          describe(actual.legs.name, () => {
+            it('should contain booster', () => {
+              expect(actual.booster.classification).toStrictEqual(booster)
+            })
+          })
+        })
+      }
+    })
+  )
+})
+
---
+++ src/core/assembly/random/random-builder.ts
@@ -0,0 +1,37 @@
+import {Assembly} from "~core/assembly/assembly.ts";
+import {Candidates} from "~core/assembly/candidates.ts";
+import {notEquipped} from "~data/booster.ts";
+
+type Randomizer = () => number
+export function randomBuild(
+  candidates: Candidates,
+  randomizer: Randomizer = () => Math.random(),
+): Assembly {
+  const randomSelect = <T>(xs: readonly T[]): T => xs[
+    Math.floor(randomizer() * (xs.length - 1))
+  ]
+
+  const legs = randomSelect(candidates.legs)
+  const base: Omit<Assembly, 'legs' | 'booster'> = {
+    rightArmUnit: randomSelect(candidates.rightArmUnits),
+    leftArmUnit: randomSelect(candidates.leftArmUnits),
+    rightBackUnit: randomSelect(candidates.rightBackUnits),
+    leftBackUnit: randomSelect(candidates.leftBackUnits),
+
+    head: randomSelect(candidates.heads),
+    core: randomSelect(candidates.cores),
+    arms: randomSelect(candidates.arms),
+
+    fcs: randomSelect(candidates.fcses),
+    generator: randomSelect(candidates.generators),
+
+    expansion: randomSelect(candidates.expansions),
+  }
+
+  switch(legs.category) {
+    case 'tank':
+      return { ...base, legs, booster: notEquipped }
+    default:
+      return { ...base, legs, booster: randomSelect(candidates.boosters) }
+  }
+}

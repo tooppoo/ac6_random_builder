55c9bcb447a6666cc7a118fde04ca4d5272f87fd
philomagi <tooppoo56@gmail.com>
2024-02-17 03:06:25 +0900

    add eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  "overrides": [],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "plugins": [
+    "@typescript-eslint"
+  ],
+  "rules": {
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": ["error", {"argsIgnorePattern": "^_"}]
+  }
+}

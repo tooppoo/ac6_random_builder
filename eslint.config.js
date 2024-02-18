be5d782f2da4e04a53ebb2cedfa321bda4b47f42
philomagi <tooppoo56@gmail.com>
2024-02-18 16:50:59 +0900

    lint チェックの範囲訂正

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,9 @@ import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
+  {
+    ignores: ['dist/**/*', 'coverage/**/*'],
+  },
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   {

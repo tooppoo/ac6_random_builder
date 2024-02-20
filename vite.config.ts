87897e37fc3fd09fc3962f325a10fe0845aa22e3
philomagi <tooppoo56@gmail.com>
2024-02-20 14:44:09 +0900

    favicon追加

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     alias: {
       '~data/': join(__dirname, 'data/'),
       '~core/': join(__dirname, 'src/core/'),
-      '~view/*': join(__dirname, 'src/view/'),
+      '~view/': join(__dirname, 'src/view/'),
     },
   },
 })

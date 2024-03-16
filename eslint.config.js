1eb380d99820980201f2154cb01ac32489dad865
philomagi <tooppoo56@gmail.com>
2024-03-16 16:56:05 +0900

    svelte用の設定追加

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,43 @@
 import eslint from '@eslint/js'
 import { FlatCompat } from '@eslint/eslintrc'
+import tsParser from '@typescript-eslint/parser'
 import globals from 'globals'
-import tseslint from 'typescript-eslint'
+import svelteParser from 'svelte-eslint-parser'
+import tsEslint from 'typescript-eslint'
 
 const compat = new FlatCompat()
 
-export default tseslint.config(
+export default tsEslint.config(
   {
     ignores: ['dist/**/*', 'coverage/**/*'],
   },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...compat.extends('plugin:svelte/recommended'),
   {
     languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+        extraFileExtensions: ['.svelte'],
+      },
       globals: {
         ...globals.browser,
         ...globals.es2021,
       },
     },
+  },
+  {
+    files: ['**/*.svelte'],
+    languageOptions: {
+      parser: svelteParser,
+      parserOptions: {
+        parser: tsParser,
+      },
+    },
+  },
+  eslint.configs.recommended,
+  ...tsEslint.configs.recommended,
+  ...compat.extends('plugin:svelte/recommended'),
+  {
     rules: {
       'no-unused-vars': 'off',
       '@typescript-eslint/no-unused-vars': [

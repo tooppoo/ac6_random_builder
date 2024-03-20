e3e177c5a359ff018b57cc39be864656f700c6f0
philomagi <tooppoo56@gmail.com>
2024-03-20 18:47:01 +0900

    chore(ui): candidatesをジェネレータ経由で生成する形に変更

--- spec/helper.ts
+++ spec/helper.ts
@@ -1,24 +1,16 @@
-import fc, { type ArrayConstraints } from 'fast-check'
+import fc, { Arbitrary, type ArrayConstraints } from 'fast-check'
 import type { RawAssembly } from '~core/assembly/assembly.ts'
 import { LockedParts } from '~core/assembly/random/lock.ts'
 import { randomBuild } from '~core/assembly/random/random-builder.ts'
 import type { Candidates } from '~data/types/candidates.ts'
 import { candidates } from '~data/versions/v1.06.1.ts'
 
-export function genRandomizer() {
-  return fc.float({ min: 0, max: 1, noNaN: true, noDefaultInfinity: true })
-}
-
-export function genAssembly(c: Candidates = candidates) {
-  return genRandomizer().map((i) => randomBuild(c, { randomizer: () => i }))
-}
-export function genAssemblyKeys(
-  cons: ArrayConstraints & { candidates?: Candidates },
-) {
-  return genAssembly(cons.candidates || candidates).chain((a) =>
+export const genAssembly = (candidates: Candidates | null = null) =>
+  (candidates ? fc.constant(candidates) : genCandidates()).map(randomBuild)
+export const genAssemblyKeys = (cons: ArrayConstraints) =>
+  genAssembly().chain((a) =>
     fc.array(fc.oneof(...a.keys.map(fc.constant)), cons),
   )
-}
 
 export const genLockedParts = () =>
   genAssemblyPartWithKeyPairs().map((pairs) => ({
@@ -33,10 +25,10 @@ const genAssemblyPartWithKeyPairs = () =>
     .chain((assembly) =>
       fc.record({
         assembly: fc.constant(assembly as RawAssembly),
-        keys: fc.uniqueArray(
-          fc.oneof(...assembly.keys.map((k) => fc.constant(k))),
-          { minLength: 0, maxLength: assembly.keys.length },
-        ),
+        keys: fc.uniqueArray(fc.constantFrom(...assembly.keys), {
+          minLength: 0,
+          maxLength: assembly.keys.length,
+        }),
       }),
     )
     .map(({ assembly, keys }) =>
@@ -45,3 +37,32 @@ const genAssemblyPartWithKeyPairs = () =>
         part: assembly[key],
       })),
     )
+
+export const genCandidates = (() => {
+  const toArray =
+    (constraints: ArrayConstraints) =>
+    <T>(xs: readonly T[]): Arbitrary<T[]> =>
+      fc.uniqueArray(fc.constantFrom(...xs), constraints)
+
+  return (constraints: ArrayConstraints = { minLength: 1 }) => {
+    const toArr = toArray(constraints)
+
+    return fc.record<Candidates>({
+      rightArmUnits: toArr(candidates.rightArmUnits),
+      leftArmUnits: toArr(candidates.leftArmUnits),
+      rightBackUnits: toArr(candidates.rightBackUnits),
+      leftBackUnits: toArr(candidates.leftBackUnits),
+
+      heads: toArr(candidates.heads),
+      cores: toArr(candidates.cores),
+      arms: toArr(candidates.arms),
+      legs: toArr(candidates.legs),
+
+      boosters: toArr(candidates.boosters),
+      fcses: toArr(candidates.fcses),
+      generators: toArr(candidates.generators),
+
+      expansions: toArr(candidates.expansions),
+    })
+  }
+})()

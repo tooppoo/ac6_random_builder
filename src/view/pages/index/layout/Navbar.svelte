cd32626ede504b47fb95df5710626cb6f114eebc
philomagi <tooppoo56@gmail.com>
2024-03-26 00:55:47 +0900

    fix(ui): 参照パスをローカルでも本番でも動作するように変更

--- src/view/pages/index/layout/Navbar.svelte
+++ src/view/pages/index/layout/Navbar.svelte
@@ -1,7 +1,7 @@
 
 <nav class="navbar navbar-expand-lg sticky-top bg-body-tertiary border-bottom border-secondary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="/">
+    <a class="navbar-brand" href={import.meta.env.BASE_URL}>
       Ⅵ
     </a>
     <div class="navbar-nav ms-auto d-flex flex-row">

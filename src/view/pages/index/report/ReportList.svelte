5e2ac26fc5f136f6185c07fe81dacb678df9593a
philomagi <tooppoo56@gmail.com>
2024-04-19 02:05:58 +0900

    reportのレイアウトを調整

--- src/view/pages/index/report/ReportList.svelte
+++ src/view/pages/index/report/ReportList.svelte
@@ -6,31 +6,44 @@
 
   export let assembly: Assembly
 
-  let reportItems: ReadonlyArray<{
+  type ReportItems = ReadonlyArray<{
     key: Exclude<keyof AssemblyProperty, 'withinEnOutput' | 'withinLoadLimit'>,
     status: ReportStatus
-  }> = [
-    { key: 'ap', status: 'normal' },
-    { key: 'weight', status: 'normal' },
-    { key: 'load', status: assembly.withinLoadLimit ? 'normal' : 'danger' },
-    { key: 'loadLimit', status: assembly.withinLoadLimit ? 'normal' : 'danger' },
-    { key: 'enLoad', status: assembly.withinEnOutput ? 'normal' : 'danger' },
-    { key: 'enOutput', status: assembly.withinEnOutput ? 'normal' : 'danger' },
-    { key: 'enSurplus', status: assembly.withinEnOutput ? 'normal' : 'danger' },
-    { key: 'enSupplyEfficiency', status: assembly.withinEnOutput ? 'normal' : 'danger' },
-    { key: 'enRechargeDelay', status: assembly.withinEnOutput ? 'normal' : 'danger' },
-    { key: 'coam', status: 'normal' },
+  }> 
+  let reportItems: ReportItems[]= [
+    [
+      { key: 'ap', status: 'normal' },
+      { key: 'coam', status: 'normal' },
+    ],
+    [
+      { key: 'weight', status: 'normal' },
+      { key: 'load', status: assembly.withinLoadLimit ? 'normal' : 'danger' },
+      { key: 'loadLimit', status: assembly.withinLoadLimit ? 'normal' : 'danger' },
+    ],
+    [
+      { key: 'enLoad', status: assembly.withinEnOutput ? 'normal' : 'danger' },
+      { key: 'enOutput', status: assembly.withinEnOutput ? 'normal' : 'danger' },
+      { key: 'enSurplus', status: assembly.withinEnOutput ? 'normal' : 'danger' },
+      { key: 'enSupplyEfficiency', status: assembly.withinEnOutput ? 'normal' : 'danger' },
+      { key: 'enRechargeDelay', status: assembly.withinEnOutput ? 'normal' : 'danger' },
+    ],
   ]
 
 </script>
 
-<div class="row mb-3">
-  {#each reportItems as { key, status }}
-    <ReportItem
-      caption={$i18n.t(key, { ns: 'assembly' })}
-      class="mb-3"
-      value={assembly[key]}
-      status={status}
-    />
-  {/each}
-</div>
+{#each reportItems as xs, i}
+  {#if (i !== 0)}
+    <hr>
+  {/if}
+  <div>
+    <div class="row mb-3">
+      {#each xs as { key, status }}
+        <ReportItem
+          caption={$i18n.t(key, { ns: 'assembly' })}
+          value={assembly[key]}
+          status={status}
+        />
+      {/each}
+    </div>
+  </div>
+{/each}

c4fccdd6f4c48bcd29adcfa7c6e5d37aaac2a2f2
philomagi <tooppoo56@gmail.com>
2024-04-20 04:32:23 +0900

    feat(ui): containerオブジェクトを追加

--- src/view/pages/index/report/ReportList.svelte
+++ src/view/pages/index/report/ReportList.svelte
@@ -1,34 +1,15 @@
 <script lang="ts">
   import type { Assembly } from '~core/assembly/assembly'
 
-  import i18n from "~view/i18n/define.ts";
-  // import type { ReportStatus } from '~view/pages/index/report/ReportItem.svelte'
   import { defaultReportAggregation } from '~view/pages/index/report/model/report'
-  import ReportItem from '~view/pages/index/report/ReportItem.svelte'
+  import ReportListViewer from '~view/pages/index/report/ReportListViewer.svelte'
 
   export let assembly: Assembly
 
   let reportAggregation = defaultReportAggregation()
 </script>
 
-{#each reportAggregation.blocks as block}
-  <hr>
-  <div>
-    <div class="row mb-3">
-      {#each block.reports as report}
-        <ReportItem
-          caption={$i18n.t(report.key, { ns: 'assembly' })}
-          class="mb-3"
-          value={assembly[report.key]}
-          status={report.statusFor(assembly)}
-        />
-      {/each}
-    </div>
-  </div>
-{/each}
-
-<style scoped>
-  hr:first-of-type {
-    display: none;
-  }
-</style>
+<ReportListViewer
+  assembly={assembly}
+  reportAggregation={reportAggregation}
+/>

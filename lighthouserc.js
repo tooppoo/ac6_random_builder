1a7f747bb3f2403384a12c19c2f187c70ee8eb0f
philomagi <tooppoo56@gmail.com>
2024-04-21 11:01:13 +0900

    ci: config追加

---
+++ lighthouserc.js
@@ -0,0 +1,9 @@
+export default  {
+  collect: {
+    url: ['http://localhost:4173/'],
+    startServerCommand: 'npm run preview',
+  },
+  upload: {
+    target: 'temporary-public-storage',
+  },
+};

55058a0fca6441c452541a772d4c35c2320be0c0
philomagi <tooppoo56@gmail.com>
2024-05-19 14:03:28 +0900

    feat(ui): ハンドラを関数に分離

--- src/view/components/language/LanguageForm.svelte
+++ src/view/components/language/LanguageForm.svelte
@@ -1,14 +1,19 @@
 
 <script lang="ts">
   import i18n from "~view/i18n/define.ts";
+
+  function onChange(e: Event) {
+    const target = e.target as HTMLInputElement
+    const language = target.value
+
+    $i18n.changeLanguage(language)
+  }
 </script>
 
 <div>
   <select
     id="change-language"
-    on:change={({ target }) => {
-      $i18n.changeLanguage(target.value)
-    }}
+    on:change={onChange}
   >
     <option value="ja" selected>日本語</option>
     <option value="en">English</option>

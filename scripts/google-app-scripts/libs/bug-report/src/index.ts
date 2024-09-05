c7c3d7705f5c48be90c5e88b3ddcaaa36bb5151b
philomagi <tooppoo56@gmail.com>
2024-09-06 02:35:38 +0900

    build: #130 リクエスト送信方法を修正

--- scripts/google-app-scripts/libs/bug-report/src/index.ts
+++ scripts/google-app-scripts/libs/bug-report/src/index.ts
@@ -3,18 +3,27 @@ import { createIssue } from 'share/github'
 type FormsOnFormSubmit = GoogleAppsScript.Events.FormsOnFormSubmit
 
 async function onFormSubmit(e: FormsOnFormSubmit): Promise<void> {
-  const [feature, summary, detail] = e.response.getItemResponses()
+  const [feature, summary, detail, ...medias] = e.response.getItemResponses()
 
   const title = `[BUG] ${summary.getResponse()}`
   const text = `
 # 機能
 ${feature.getResponse()}
 
-# 詳細
+# 再現手順
 ${detail.getResponse()}
+
+# 発生時の様子
 `
   // https://docs.github.com/ja/rest/issues/issues?apiVersion=2022-11-28#create-an-issue
   await createIssue({ title, text })
+
+  medias.forEach((m) => {
+    console.log(m.getItem().getId())
+    console.log(m.getItem().getTitle())
+    console.log(m.getResponse())
+    console.log('-------------')
+  })
 }
 
 ScriptApp.newTrigger(onFormSubmit.name)
--- scripts/google-app-scripts/share/github.ts
+++ scripts/google-app-scripts/share/github.ts
@@ -1,25 +1,22 @@
-const githubToken = import.meta.env.VITE_ISSUE_POST_TOKEN ?? ''
-const githubRepository = import.meta.env.VITE_REPO_NAME ?? ''
-
 type CreateIssueArgs = Readonly<{
   title: string
   text: string
 }>
-export function createIssue({
-  title,
-  text,
-}: CreateIssueArgs): Promise<unknown> {
-  return fetch(`https://api.github.com/repos/${githubRepository}/issues`, {
-    method: 'POST',
-    headers: {
-      Accept: 'application/vnd.github+json',
-      Authorization: `Bearer ${githubToken}`,
-      'X-GitHub-Api-Version': '2022-11-28',
+export function createIssue({ title, text }: CreateIssueArgs): unknown {
+  return UrlFetchApp.fetch(
+    `https://api.github.com/repos/${import.meta.env.VITE_REPO_NAME}/issues`,
+    {
+      method: 'post',
+      headers: {
+        Accept: 'application/vnd.github+json',
+        Authorization: `Bearer ${import.meta.env.VITE_ISSUE_POST_TOKEN}`,
+        'X-GitHub-Api-Version': '2022-11-28',
+      },
+      payload: JSON.stringify({
+        title,
+        body: text,
+        labels: ['bug'],
+      }),
     },
-    body: JSON.stringify({
-      title,
-      body: text,
-      labels: ['bug'],
-    }),
-  })
+  )
 }

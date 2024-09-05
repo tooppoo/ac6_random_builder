6c0ef0c042ae1fc8d999cac6b29351a53bd2732e
philomagi <tooppoo56@gmail.com>
2024-09-06 03:36:45 +0900

    build: #130 画像へのリンクを挿入

--- scripts/google-app-scripts/libs/bug-report/src/index.ts
+++ scripts/google-app-scripts/libs/bug-report/src/index.ts
@@ -3,27 +3,34 @@ import { createIssue } from 'share/github'
 type FormsOnFormSubmit = GoogleAppsScript.Events.FormsOnFormSubmit
 
 async function onFormSubmit(e: FormsOnFormSubmit): Promise<void> {
-  const [feature, summary, detail, ...medias] = e.response.getItemResponses()
+  const [feature, summary, detail, media] = e.response.getItemResponses()
 
   const title = `[BUG] ${summary.getResponse()}`
+  const mediaLinks = (() => {
+    const res = media.getResponse()
+    const prefix = 'https://drive.google.com/open?id='
+
+    if (!Array.isArray(res)) {
+      return [`${prefix}${res}`]
+    }
+
+    return res.map((id) => `${prefix}${id}`)
+  })()
   const text = `
-# 機能
+# 不具合のあった機能
 ${feature.getResponse()}
+
+# 不具合のあった機能
+${summary.getResponse()}
 
 # 再現手順
 ${detail.getResponse()}
 
 # 発生時の様子
+${mediaLinks.map((l) => `- ${l}`).join('\n')}
 `
   // https://docs.github.com/ja/rest/issues/issues?apiVersion=2022-11-28#create-an-issue
   await createIssue({ title, text })
-
-  medias.forEach((m) => {
-    console.log(m.getItem().getId())
-    console.log(m.getItem().getTitle())
-    console.log(m.getResponse())
-    console.log('-------------')
-  })
 }
 
 ScriptApp.newTrigger(onFormSubmit.name)

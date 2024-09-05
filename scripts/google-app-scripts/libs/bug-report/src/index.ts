e47a94f9164ea1ed440480e96e89433892eb81a6
philomagi <tooppoo56@gmail.com>
2024-09-06 01:21:47 +0900

    build: #130 issue投稿リクエスト

--- scripts/google-app-scripts/libs/bug-report/src/index.ts
+++ scripts/google-app-scripts/libs/bug-report/src/index.ts
@@ -1,28 +1,39 @@
-// import { Octokit } from 'octokit'
-//
-// import {
-//   githubToken,
-//   githubOwner,
-//   githubRepository,
-// } from '../../../share/github'
+import { Octokit } from 'octokit'
+
+import {
+  githubToken,
+  githubOwner,
+  githubRepository,
+} from '../../../share/github.ts'
 
 type FormsOnFormSubmit = GoogleAppsScript.Events.FormsOnFormSubmit
 
 async function onFormSubmit(e: FormsOnFormSubmit): Promise<void> {
-  const answers = e.response.getItemResponses().map((r) => r.getItem())
+  const [feature, summary, detail] = e.response.getItemResponses()
 
+  const title = `[BUG] ${summary.getResponse()}`
+  const body = `
+# 機能
+${feature.getResponse()}
+
+# 詳細
+${detail.getResponse()}
+`
   // https://docs.github.com/ja/rest/issues/issues?apiVersion=2022-11-28#create-an-issue
-  // const octokit = new Octokit({
-  //   auth: githubToken,
-  // })
-  //
-  // octokit.request(`POST /repos/${githubOwner}/${githubRepository}/issues`, {
-  //   owner: githubOwner,
-  //   repo: githubRepository,
-  //   title: `[BUG]`,
-  //   labels: ['bug'],
-  // })
-  console.log(JSON.stringify(answers))
+  const octokit = new Octokit({
+    auth: githubToken,
+  })
+
+  await octokit.request(
+    `POST /repos/${githubOwner}/${githubRepository}/issues`,
+    {
+      owner: githubOwner,
+      repo: githubRepository,
+      title,
+      body,
+      labels: ['bug'],
+    },
+  )
 }
 
 ScriptApp.newTrigger(onFormSubmit.name)

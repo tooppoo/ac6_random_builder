47a6a5c12c19dfcd2a92d1f394d2fc8f8596b75c
philomagi <tooppoo56@gmail.com>
2024-09-06 12:32:30 +0900

    build: #130 remove tag if exist

--- scripts/google-app-scripts/release/release-gas.sh
+++ scripts/google-app-scripts/release/release-gas.sh
@@ -16,5 +16,12 @@ fi
 
 tag="google-app-scripts/$target/$version"
 
+echo "git tag --delete $tag"
+git tag --delete "$tag" || true
+echo "git push --delete origin $tag"
+git push --delete origin "$tag" || true
+
+echo "git tag $tag"
 git tag "$tag"
-git push origin "$tag" || true
+echo "git push origin $tag"
+git push origin "$tag"

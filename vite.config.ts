b7308c96147ec18fafa0730f337fa6de75f04199
philomagi <tooppoo56@gmail.com>
2024-09-07 23:10:36 +0900

    feat: #303 bundle analyzer 導入

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { join } from 'path'
 
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { defineConfig } from 'vite'
+import { analyzer } from "vite-bundle-analyzer";
 import dynamicImport from 'vite-plugin-dynamic-import'
 import pluginPurgeCss from 'vite-plugin-purgecss-updated-v5'
 import Sitemap from 'vite-plugin-sitemap'
@@ -17,6 +18,26 @@ export default defineConfig({
     pluginPurgeCss({
       variables: true,
     }),
+    (() => {
+      if (process.env.WITH_ANALYZE !== 'true') return null
+
+      switch (process.env.ANALIZE_MODE) {
+        case 'server':
+          return analyzer({
+            analyzerMode: 'server',
+            openAnalyzer: true,
+            summary: true,
+          })
+        case 'static':
+        case 'json':
+          return analyzer({
+            analyzerMode: process.env.ANALIZE_MODE || 'static',
+            fileName: '../analyze',
+          })
+        default:
+          return null
+      }
+    })()
   ],
   resolve: {
     alias: {

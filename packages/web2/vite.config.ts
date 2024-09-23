0907bba9448e804101f8822816a2e2ae0580e7d3
philomagi <tooppoo56@gmail.com>
2024-09-23 14:21:11 +0900

    feat: #328  build用ライブラリ追加

--- packages/web2/vite.config.ts
+++ packages/web2/vite.config.ts
@@ -1,8 +1,39 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+import { sveltekit } from '@sveltejs/kit/vite'
+import { defineConfig } from 'vitest/config'
+import { analyzer } from 'vite-bundle-analyzer'
+import pluginPurgeCss from 'vite-plugin-purgecss-updated-v5'
+import Sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+    Sitemap({
+      hostname: `https://tooppoo.github.io/`,
+      basePath: '/ac6_assemble_tool',
+    }),
+    pluginPurgeCss({
+      variables: true,
+    }),
+    (() => {
+      console.log('ANALYZE_MODE=', process.env.ANALYZE_MODE)
+      switch (process.env.ANALYZE_MODE) {
+        case 'server':
+          return analyzer({
+            analyzerMode: 'server',
+            openAnalyzer: true,
+            summary: true,
+          })
+        case 'static':
+        case 'json':
+          return analyzer({
+            analyzerMode: process.env.ANALYZE_MODE,
+            fileName: '../analyze',
+          })
+        default:
+          return null
+      }
+    })(),
+  ],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
